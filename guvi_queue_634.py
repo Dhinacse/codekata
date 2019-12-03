16388b5f9b2729405b805ed765cdf6877eae331e
dhina <46160908+Dhinacse@users.noreply.github.com>
2019-12-03 20:03:37 +0530

    Create guvi_queue_634.py

---
+++ guvi_queue_634.py
@@ -0,0 +1,17 @@
+s = 'I am john cena cena john'
+L = input().split()
+if L == s.split() :
+    print('I am',end='')
+else :
+    i=0
+    while True :
+        if i >= len(L)-1 :
+            break
+        if L[i] == L[i+1] :
+            x = L[i]
+            L.remove(x)
+            L.remove(x)
+            i = 0
+        else :
+            i += 1
+    print(*L,end='')
